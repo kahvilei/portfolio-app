3f4d5dd2e4335133f4939018eff1829f6b09ce64
kahvilei <luovakatie@gmail.com>
2019-11-10 12:18:14 -0600

    Updates

---
+++ precache-manifest.ce6a256be971ddd3cee4229233728adc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a077bc7f29568b4f45076b2d4dba8da9",
+    "url": "/portfolio-app/index.html"
+  },
+  {
+    "revision": "c7db4b0058f1b5417ba0",
+    "url": "/portfolio-app/static/css/main.81869642.chunk.css"
+  },
+  {
+    "revision": "d8d5138d83076bb063fc",
+    "url": "/portfolio-app/static/js/2.49dee8d7.chunk.js"
+  },
+  {
+    "revision": "c7db4b0058f1b5417ba0",
+    "url": "/portfolio-app/static/js/main.379de1f0.chunk.js"
+  },
+  {
+    "revision": "e9f3188cdfb1f80a93a2",
+    "url": "/portfolio-app/static/js/runtime-main.3084481c.js"
+  }
+]);
